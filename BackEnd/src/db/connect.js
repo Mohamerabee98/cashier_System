0a67cce758c550e026e26111db94fad1dbf9fccb
Mohamerabee98 <Morabee221133@outlook.com>
2026-03-17 15:48:13 +0200

    Donegit add .

--- BackEnd/src/db/connect.js
+++ BackEnd/src/db/connect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDB() {
  await mongoose
-    .connect("mongodb://localhost:27017/kashire_System")
+    .connect("mongodb://localhost:27017/cashier_System")
     .then(() => {
       console.log("db connected successfully");
     })
